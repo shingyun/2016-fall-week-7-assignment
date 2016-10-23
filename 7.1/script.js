60faa953bb77972aa7fce9e82846b4a6423f0218
Shing-Yun Chiang <smilein0119@gmail.com>
2016-10-23 17:46:29 -0400

    assignment 1st version

--- 7.1/script.js
+++ 7.1/script.js
@@ -18,11 +18,14 @@ var scaleX, scaleY;
 
 //Step 1: importing multiple datasets
 d3.queue()
-    //.defer()
-    //.defer()
-    //.defer()
+    .defer(d3.csv,"../data/olympic_medal_count_1900.csv",parse)
+    .defer(d3.csv,"../data/olympic_medal_count_1960.csv",parse)
+    .defer(d3.csv,"../data/olympic_medal_count_2012.csv",parse)
     .await(function(err,rows1900,rows1960,rows2012){
+        
+
         //Draw axis
+
         scaleY = d3.scaleLinear()
             .domain([0,120])
             .range([h,0]);
@@ -43,16 +46,59 @@ d3.queue()
         d3.select('#year-1900').on('click', function(){
             draw(rows1900);
         });
+        d3.select('#year-1960').on('click', function(){
+            draw(rows1960);
+        });
+        d3.select('#year-2012').on('click', function(){
+            draw(rows2012);
+        });
 
     });
 
 //Step 3: implement the enter / exit / update pattern
 function draw(rows){
     var top5 = rows.sort(function(a,b){
-        return b.count2012 - a.count2012;
+        return b.count - a.count;
     }).slice(0,5);
 
-    console.log(top5);
+    console.table(top5);
+
+    //Represent: nodes
+    var bars = plot.selectAll('rect')
+          .data(top5);//Update
+
+    var text = plot.selectAll('.countryText')
+          .data(top5);
+
+    //Enter
+    var barsEnter = bars.enter()
+          .append('g')
+          .attr('class','bars')
+          .append('rect');
+
+    var textEnter = text.enter()
+          .append('g')
+          .attr('class','country')
+          .append('text')
+          .attr('class','countryText')
+          .attr('transform',function(d,i){
+            return 'translate('+ scaleX(i)+','+(h+20)+')';
+          });
+
+    //Exit
+    bars.exit().remove();
+    text.exit().remove();
+
+    //Update
+    bars.merge(barsEnter)
+        .attr('x',function(d,i){return scaleX(i)})
+        .attr('y',function(d){return scaleY(d.count)})
+        .attr('width',50)
+        .attr('height',function(d){return h-scaleY(d.count)});
+
+    text.merge(textEnter)
+        .text(function(d){return d.country});
+
 }
 
 function parse(d){
